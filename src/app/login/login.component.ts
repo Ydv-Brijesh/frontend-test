0041a68432b10f670cc96213b31375dc5511b636
Ydv-Brijesh <brijesh.yadav@troology.com>
2022-09-14 17:01:42 +0530

    awergfw45gw46hw

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,50 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormControlName, Validators, FormGroup } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  auth: any
   hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
-  name = new FormControl('', [Validators.required]);
-  number = new FormControl('', [Validators.required]);
-  password = new FormControl('', [Validators.required]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a email';
-    }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  };
-
-  getErrorMessagename() {
-    if (this.name.hasError('required')) {
-      return 'You must enter a name';
-    }
-    return this.name.hasError('name') ? 'Not a valid name' : '';
-  }
-  
-  getErrorMessagenumber() {
-    if (this.number.hasError('required')) {
-      return 'You must enter a number';
-    }
-    return this.number.hasError('number') ? 'Not a valid number' : '';
-  }
-
-  getErrorMessagepassword() {
-    if (this.password.hasError('required')) {
-      return 'You must enter a password';
-    }
-    return this.password.hasError('password') ? 'Not a valid password' : '';
-  }
 
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    user: new FormControl('', [Validators.required]),
+    number: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+
+  })
+
+  constructor(private authservice: AuthService) { }
+
 
   ngOnInit(): void {
   }
 
+  loginUser() {
+    console.log(this.loginForm.valid);
+
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
